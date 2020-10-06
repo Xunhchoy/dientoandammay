a3e64f45fd451425db744afe59c81530e1ac2620
Xunhchoy <dohai03052000@gmail.com>
2020-10-06 08:41:19 +0700

    add login.php for githubz

---
+++ login.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+ <p>
+ Trang login
+ </p>
+</head>
+<body>
+<div class="row">
+                <form method="post">
+                    Ten dang nhap:
+                    <input type="text" name="tendn">
+                    Password:
+                    <input type="text" name="ps">
+                    <input type="submit" name="dangnhap" value="dang nhap">
+                </form>
+            </div>
+</body>
+</html>
